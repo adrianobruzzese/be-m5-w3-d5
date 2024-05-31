3323d2d20ce1646f8ba5875f472ac636cde0448f
Adriano Bruzzese <146066663+adrianobruzzese@users.noreply.github.com>
2024-05-31 15:57:37 +0200

    almost finished project

---
+++ src/test/java/adrianobruzzese/bem5w3d5/BeM5W3D5ApplicationTests.java
@@ -0,0 +1,14 @@
+package adrianobruzzese.bem5w3d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeM5W3D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Questo test verifica che il contesto dell'applicazione si carichi correttamente
+	}
+
+}
